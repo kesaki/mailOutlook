3e91f2a3cd9543aef5fd6ee36fe66b51675841be
kesaki <64cr12@gmail.com>
2023-02-11 16:44:34 +0900

    gitinit

---
+++ mail.py
@@ -0,0 +1,38 @@
+import xlrd
+import datetime
+file = "sample.xls"
+wb = xlrd.open_workbook(file)
+ws1 = wb.sheet_by_index(0)
+
+def get_row_no(key):
+  for r in range(ws1.nrows):
+    try:
+      number = str(int(ws1.cell(r,0).value))
+    except:
+      continue
+    if number == key:
+      return(r)
+    
+def ser_to_dt(serialVal:float) -> datetime:
+    dtime = (datetime.datetime(1899,12,30) + datetime.timedelta(serialVal))
+    return dtime
+
+def get_mail(number:str):
+  try:
+    row_no = get_row_no(number)
+    salon_name = ws1.cell(row_no, 1).value
+    sagyou_date = ser_to_dt(ws1.cell(row_no, 2).value).strftime("%Y/%m/%d")
+    nyuten_time = ser_to_dt(ws1.cell_value(row_no, 3)).strftime("%H:%M")
+    sagyo_kanryo_time = ser_to_dt(ws1.cell_value(row_no, 4)).strftime("%H:%M")
+    atesaki = "atesaki@mail"
+    cc_atesaki = "cc_atesaki@mail"
+    title = f"{salon_name}の作業報告"
+    mail = f"""店名:{salon_name}
+日付:{sagyou_date}
+入店時刻：{nyuten_time}
+退店時刻：{sagyo_kanryo_time}
+"""
+    return {"atesaki": atesaki, "cc_atesaki": cc_atesaki, "title": title, "mail": mail}
+  except:
+    return "行が見つかりませんでした"
+
---
+++ outlook.py
@@ -0,0 +1,42 @@
+
+number = input("番号")
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
+
+
+import mail as module
+mail_data = module.get_mail(number)
+if type(mail_data) != dict:
+  print(mail_data)
+  sleep(2)
+  exit ()
+driver = webdriver.Chrome("./chromedriver", options=options)
+driver.implicitly_wait(10)
+wait = WebDriverWait(driver, 10)
+
+driver.get("https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1675938175&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26RpsCsrfState%3d686921c1-d52b-5d19-d63b-f5d4dfa2bb57&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015")
+driver.find_element(By.CSS_SELECTOR, 'input[type="email"]').send_keys("64")
+driver.find_element(By.ID, "idSIButton9").click()
+driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').send_keys("Ume")
+wait.until(expected_conditions.visibility_of_element_located((By.ID, "idSIButton9")))
+# wait.until(expected_conditions.element_to_be_clickable((By.ID, "idSIButton9")))
+driver.find_element(By.ID, "idSIButton9").click()
+driver.find_element(By.ID, "idSIButton9").click()
+
+wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, "div[aria-label='新規メール']")))
+# wait.until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, "div[aria-label='新規メール']")))
+# sleep(3)
+driver.find_element(By.CSS_SELECTOR, "div[aria-label='新規メール']").click()
+driver.find_element(By.XPATH, "//*[@class='QHw8J PCB1B bzocG Viglg']/button[1]").click()
+
+driver.find_element(By.CSS_SELECTOR, "div[class='VbY1P T6Va1 Z4n09 EditorClass']").send_keys(mail_data["atesaki"])
+driver.find_element(By.CSS_SELECTOR, "div[aria-label='CC']").send_keys(mail_data["cc_atesaki"])
+driver.find_element(By.XPATH, "//*[@class='P6mmz']/div/div/div/input").send_keys(mail_data["title"])
+driver.find_element(By.CSS_SELECTOR, "div[class='dFCbN k1Ttj dPKNh DziEn']").send_keys(mail_data["mail"])
+
+
